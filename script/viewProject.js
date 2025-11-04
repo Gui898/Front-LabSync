08c57c8531c2c2c7c952ecdf645518f22f543859
Isadora Clavé <isadoraclave@gmail.com>
2025-11-04 15:04:57 -0300

    Making the heart in the favoritesView

--- script/viewProject.js
+++ script/viewProject.js
@@ -1,6 +1,14 @@
+const loggedUser = JSON.parse(localStorage.getItem("loggedUser"));
+
 const postDetailed = JSON.parse(localStorage.getItem("currentPostView"));
 
+let allFavorites = JSON.parse(localStorage.getItem("userFavorites"));
+
+let isFavorited;
+
 const likeCount = document.getElementById("likeCount");
+const heartFav = document.getElementById("postFavoriteHeart");
+const imgFav = document.getElementById("heartImg");
 
 initializer();
 
@@ -12,6 +20,10 @@ function initializer() {
     const usedTechArea = document.getElementById("usedTech");
 
     if (postDetailed.idFavorite != null) {
+
+        isFavorited = true;
+        imgFav.src = "../assets/alreadyFavoritedHeart.png";
+
         titleArea.innerHTML = postDetailed.posts.project.title;
         categoryArea.innerHTML = postDetailed.posts.project.category;
         textArea.innerHTML = postDetailed.posts.project.textProjects;
@@ -20,6 +32,13 @@ function initializer() {
 
         likeCount.textContent = postDetailed.posts.likes;
     } else {
+
+        isFavorited = allFavorites.includes(postDetailed.idPost);
+
+        if(isFavorited){
+            imgFav.src = "../assets/alreadyFavoritedHeart.png";
+        }
+
         titleArea.innerHTML = postDetailed.project.title;
         categoryArea.innerHTML = postDetailed.project.category;
         textArea.innerHTML = postDetailed.project.textProjects;
@@ -60,6 +79,69 @@ document.getElementById("likeButton").addEventListener("click", (e) => {
             method: "PATCH",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify({ likes: postDetailed.likes }),
+        })
+            .then(async res => {
+                const data = await res.json().catch(() => null);
+                if (!res.ok) {
+                    const msg = data?.message || "Erro desconhecido ao atualizar posts";
+                    throw new Error(msg);
+                }
+                return data;
+            })
+            .catch(err => console.log(err.message));
+    }
+});
+
+heartFav.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    const audio = new Audio("../assets/audio/favoriting.mp3");
+
+    if (!isFavorited) {
+        isFavorited = true;
+        imgFav.src = "../assets/alreadyFavoritedHeart.png";
+        heartFav.classList.add("favorited__button");
+        audio.play();
+
+        let favoriteObj;
+
+        if (postDetailed.idFavorite != null) {
+            favoriteObj = {
+                user: loggedUser,
+                posts: postDetailed.posts,
+            }
+        }else{
+            favoriteObj = {
+                user: loggedUser,
+                posts: postDetailed
+            }
+        }
+
+        fetch("http://localhost:8080/favorite", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(favoriteObj),
+        })
+            .then(async res => {
+                const data = await res.json().catch(() => null);
+                if (!res.ok) {
+                    const msg = data?.message || "Erro desconhecido ao atualizar posts";
+                    throw new Error(msg);
+                }
+                return data;
+            })
+            .catch(err => console.log(err.message));
+
+    } else {
+        isFavorited = false;
+        imgFav.src = "../assets/postFavoriteHeart.png";
+        heartFav.classList.remove("favorited__button");
+
+        const idPost = postDetailed.idFavorite != null ? postDetailed.posts.idPost : postDetailed.idPost;
+
+        fetch(`http://localhost:8080/favorite/${idPost}/${loggedUser.idUser}`, {
+            method: "DELETE",
+            headers: { "Content-Type": "application/json" },
         })
             .then(async res => {
                 const data = await res.json().catch(() => null);
