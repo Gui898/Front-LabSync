aef06742940639476b62954072d589484f242b88
Gui898 <gui.miojo.lasanha@gmail.com>
2025-11-03 21:42:33 -0300

    voice recognition

--- script/commum.js
+++ script/commum.js
@@ -3,3 +3,40 @@ const userKey = "loggedUser";
 const userNameHeader = document.getElementById("userName");
 
 userNameHeader.innerText = JSON.parse(localStorage.getItem(userKey)).name || " ";
+
+//VOICE RECOGNITION
+export const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+export const recognition = new SpeechRecognition();
+const commandMap = {
+    "entrar no meu perfil": "../pages/userConfig.html",
+    "voltar para a página inicial": "../index.html",
+    "entrar nos favoritos": "../pages/favorites.html",
+    "entrar nos rascunhos": "../pages/projectDrafts.html",
+    "quero ser autor": "../pages/beAnAuthor.html"
+}
+
+recognition.lang = 'pt-Br';
+recognition.start();
+
+recognition.addEventListener('result', onSpeak);
+
+export function onSpeak(e) {
+    speech = e.results[0][0].transcript;
+    speechFunc(speech);
+}
+
+export function speechFunc(speech){
+    const normalize = speech.toLowerCase();
+
+    for(command in commandMap){
+        if(normalize.includes(command)){
+            const destination = commandMap[command];
+            window.location.href = destination
+            return;
+        }
+    }
+}
+
+recognition.addEventListener('end', () =>{
+    recognition.start();
+});
